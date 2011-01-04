8f0d59a5a0db8cbc76c713c85abad980748439ce
Kristian Nygård Jensen <freeduck@member.fsf.org>
2011-01-04 21:52:40 +0100

    Adding category as a mm relation to product

---
+++ Entity/Category.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Application\ShopBundle\Entity;
+
+/**
+ * Application\ShopBundle\Entity\Category
+ */
+class Category
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @var Application\ShopBundle\Entity\Products
+     */
+    private $categories;
+
+    /**
+     * Add categories
+     *
+     * @param Application\ShopBundle\Entity\Products $categories
+     */
+    public function addCategories(\Application\ShopBundle\Entity\Products $categories)
+    {
+        $this->categories[] = $categories;
+    }
+
+    /**
+     * Get categories
+     *
+     * @return Doctrine\Common\Collections\Collection $categories
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+}
