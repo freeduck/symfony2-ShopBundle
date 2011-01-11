63cbc57d2e26a5a35284af21d30ff0b544e28ba4
Kristian Nygård Jensen <freeduck@member.fsf.org>
2011-01-11 21:02:50 +0100

    Adding new product form

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -1,8 +1,14 @@
 <?php
 
+
+
 namespace Application\ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Application\ShopBundle\Entity\Product;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\TextField;
 
 class ProductController extends Controller
 {
@@ -11,11 +17,17 @@ class ProductController extends Controller
         return $this->render('ShopBundle:Product:list.php');
     }
 
-		public function createAction($name){
-			$product = new Product();
+		public function createAction(){
+			if ('POST' === $this->get('request')->getMethod()) {
+				var_dump($this->get('request')->request->get('product'));
+			}
+			return $this->render('ShopBundle:Product:create.php');
 		}
 
 		public function newAction(){
-			return $this->render('ShopBundle:Product:new.php');
+			$product = new Product();
+			$form = new Form('product', $product, $this->get('validator'));
+			$form->add(new TextField('name'));
+			return $this->render('ShopBundle:Product:new.php', array('form' => $form));
 		}
 }
--- Resources/views/Product/new.php
+++ Resources/views/Product/new.php
@@ -1,1 +1,11 @@
 New Product
+<form action="<?php echo $view['router']->generate('shop_product_create') ?>" method="POST">
+<formfield>
+<ul class="fields">
+<li><div class="label"><label for="name">Name:</label></div><div class="field"><input type="text" id="product_name" name="product[name]"/></div></li>
+<li><input type="submit" value="Create"/></li>
+</ul>
+</formfield>
+</form>
+
+	<?php echo $view['form']->render($form) ?>
